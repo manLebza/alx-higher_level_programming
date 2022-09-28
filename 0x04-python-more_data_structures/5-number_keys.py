62437f9f0665a9d03ed3b42f5c9d1bf58dbabbbb
manLebza <manlebza.mokoena1@gmail.com>
2022-09-28 07:17:31 -0700

    hot fix 1

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,4 @@
 #!/bin/python3
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-    
-    for i in list_keys:
-        num += 1
-
+    num = len(a_dictionary)
     return (num)
