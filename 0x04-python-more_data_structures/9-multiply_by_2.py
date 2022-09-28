dea23726f5f53df4a830749436c259e31e5ec1e1
manLebza <manlebza.mokoena1@gmail.com>
2022-09-28 07:24:46 -0700

    hot fix 1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/pyhton3
-def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    lst_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-    return (new_dir)
+def multiply_by_2(ad):
+    return {k: v*2 for (k, v) in ad.items()}
