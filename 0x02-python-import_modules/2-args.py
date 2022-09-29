95cdda8fdbaaddb637e5296b6ea77fda41e2216a
manLebza <manlebza.mokoena1@gmail.com>
2022-09-28 19:59:05 -0700

    hot fix 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import sys
-    i = len(sys.argv) -1
+    i = len(sys.argv) - 1
 
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
+        print("{} argument:".format(i))
+    else:
         print("{} argument:".format(i))
 
     if i >= 1:
