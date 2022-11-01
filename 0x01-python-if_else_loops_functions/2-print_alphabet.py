4c667ba9ea2056a15251d1d868d1f69f96a6a699
manLebza <manlebza.mokoena1@gmail.com>
2022-11-01 12:52:19 +0000

    hot fix 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 """Print the alphabet in lowercase not followed by a new line. """
 
 for letter in range(97, 123):
-    print("{}".format(char(letter)), end="")
+    print("{}".format(chr(letter)), end="")
