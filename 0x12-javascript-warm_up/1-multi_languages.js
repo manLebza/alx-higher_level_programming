eca1245bc891ef077e1d8cc1c8b45ed8281c5ff8
manLebza <manlebza.mokoena1@gmail.com>
2022-12-22 00:59:45 +0000

    hot fix task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const myVar = '"C is fun"\n"Python is Cool"\n"JavaScript is amazing"\n';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
