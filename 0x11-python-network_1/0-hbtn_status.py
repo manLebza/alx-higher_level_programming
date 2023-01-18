2437a6f324179767bd41f0ef3955f45eea510db1
MANlebza <80250912+manLebza@users.noreply.github.com>
2023-01-18 11:16:02 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Module fetches https://intranet.hbtn.io/status """
+""" Module fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
 as response:
     html = response.read()
     print('Body response:')
