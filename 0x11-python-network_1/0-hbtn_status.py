03a853fa5c43ff65a03d110bb1b900c3956c599b
manLebza <manlebza.mokoena1@gmail.com>
2023-03-29 16:39:59 +0000

    pyNetworkFix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Module fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
+from urllib.request import urlopen, Request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-as response:
-    html = response.read()
-    print('Body response:')
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
+        html = res.read()
+    print("Body response:")
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
     print('\t- utf8 content: {}'.format(html.decode("utf-8")))
